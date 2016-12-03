a60a99e17c33c8b38c4db4bdd4d80af25a576442
Freshield <zxdsw199182@gmail.com>
2016-12-02 20:06:24 -0500

    done test13

---
+++ Test13/test13.js
@@ -0,0 +1,14 @@
+/**
+ * Created by FRESHIELD on 2016/12/2.
+ */
+
+var http = require('http')
+var url = require('url')
+var util = require('util')
+
+http.createServer(function (req,res) {
+	res.writeHead(200,{'Content-Type':'text/plain'})
+	res.end(util.inspect(url.parse(req.url,true)))
+}).listen(8888)
+
+
